49bbb894a4ec024d23e9b42f0c56fdff7bba0378
jlayala1016-ux <jlayala1016@gmail.com>
2026-01-31 18:10:38 +0000

    dog completed

---
+++ my-dog.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
+
+function draw() {
+  background(40000);
+  background(0,100,1000);
+  //Red Green Blue-Background
+  fill("brown")
+  circle(-60,-25,50);
+ ellipse (0,0,120,50);
+rect (-45,15,10,35);
+  rect (-30,15,10,35);
+  rect(30,15,10,35);
+  rect(15,15,10,35);
+  rect(50,-50,10,50);
+  ellipse (-40,-25,20,40);
+  ellipse(-85,-25,20,40);
+  fill("white")
+  circle(-55,-28,5)
+  circle(-70,-28,5)
+  triangle (-62.5,-20,-63,-17,-65,-23)
+  fill("black")
+  circle(-40,50,10)
+  circle(-25,50,10)
+  circle(20,50,10)
+  circle (35,50,10)
+}
